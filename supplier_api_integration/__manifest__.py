bdbc684039d54f693f26e64c43eeded4ae2e080f
softg-deve <support@softg.dev>
2026-06-01 10:39:28 -0400

    Update __manifest__.py

--- supplier_api_integration/__manifest__.py
+++ supplier_api_integration/__manifest__.py
@@ -67,6 +67,6 @@ Requirements
     'images': ['static/description/banner.gif'],
     'post_init_hook': 'post_init_hook',
     'pre_uninstall_hook': 'pre_uninstall_hook',
-    'price': 242.00,
+    'price': 60.00,
     'currency': 'EUR',
 }
